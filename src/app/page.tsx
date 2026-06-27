c02213cc48170dbcbd4e242c1777a5364ab6c3ce
Abhiiii-001 <jaiswal2abhi1947@gmail.com>
2026-06-27 17:37:32 +0530

    Fix overflow issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,20 @@ import HeroSection from './_components/HeroSection';
 
 export default function Home() {
   return (
-   <>
-    <main className="flex items-center w-full md:overflow-hidden md:px-6">
-      <AuroraBackground />
-      <Navbar />
-      
-      <div className="z-10 flex flex-col w-full md:flex-row items-center min-h-screen pt-28">
-       <div className='flex-1 md:flex-2'>
-         <HeroSection />
-       </div>
-        <div className="flex-1 pb-12 md:pb-0">
-         <TransferCard />
+    <>
+      <main className="flex items-center w-full h-screen overflow-y-auto md:overflow-hidden md:px-6">
+        <AuroraBackground />
+        <Navbar />
+
+        <div className="z-10 flex flex-col w-full md:flex-row items-center pt-28">
+          <div className='flex-1 md:flex-2'>
+            <HeroSection />
+          </div>
+          <div className="flex-1 pb-12 md:pb-0">
+            <TransferCard />
+          </div>
         </div>
-      </div>
-    </main>
-   </>
+      </main>
+    </>
   );
 }
