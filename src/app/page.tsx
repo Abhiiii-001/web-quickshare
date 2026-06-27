08c56ccfb5af2e718440b276b0098c46b2e5260b
Abhiiii-001 <jaiswal2abhi1947@gmail.com>
2026-06-27 23:24:56 +0530

    Fix UI break on mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import AuroraBackground from './_components/AuroraBackground';
-import Navbar from './_components/NavBar';
-import TransferCard from './_components/TransferCard';
-import HeroSection from './_components/HeroSection';
+import AuroraBackground from "./_components/AuroraBackground";
+import Navbar from "./_components/NavBar";
+import TransferCard from "./_components/TransferCard";
+import HeroSection from "./_components/HeroSection";
 
 export default function Home() {
   return (
     <>
-      <main className="flex items-center w-full h-screen overflow-y-auto md:overflow-hidden md:px-6">
+      <main className="flex items-center w-full md:h-screen overflow-y-auto md:overflow-hidden md:px-6">
         <AuroraBackground />
         <Navbar />
 
         <div className="z-10 flex flex-col w-full md:flex-row items-center pt-28">
-          <div className='flex-1 md:flex-2'>
+          <div className="flex-1 md:flex-2">
             <HeroSection />
           </div>
           <div className="flex-1 pb-12 md:pb-0">
